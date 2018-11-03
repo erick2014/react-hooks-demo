63e11c56a64e2a509ed1b43824afd8bf271d154c
erick <erickarmandogarcia@hotmail.com>
2018-11-03 15:46:16 -0500

    add useEffect hook to interact with window.title

--- src/app/components/Home/index.jsx
+++ src/app/components/Home/index.jsx
@@ -1,15 +1,30 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 function Home(props) {
-  const [name, setName] = useState('Erick');
+  const [name, setName] = useState('Erick')
+  const [sureName, setSureName] = useState('Armando')
+
+  useEffect(() => {
+    document.title = name + ' ' + sureName
+  })
 
   function handleNameChange(e) {
     setName(e.target.value)
   }
+
+  function handleSureNameChange(e) {
+    setSureName(e.target.value);
+  }
+
   return (
     <div>
-      <input value={name} onChange={handleNameChange} />
-      <h1>My name is {name} </h1>
+      <div>
+        <input value={name} onChange={handleNameChange} />
+      </div>
+      <div>
+        <input value={sureName} onChange={handleSureNameChange} />
+      </div>
+      <h1>My name is {name} {sureName}</h1>
     </div>
   )
 }
