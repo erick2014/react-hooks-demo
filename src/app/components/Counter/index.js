236dcf24bd5aa7ee67a1db86146897f6e7600943
erick <egarcia@arrivelogistics.com>
2018-11-08 19:27:22 -0500

    Implement effect to change document title

--- src/app/components/Counter/index.js
+++ src/app/components/Counter/index.js
@@ -1,10 +1,20 @@
 /* eslint space-before-function-paren: [2, "never"] */
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 function Counter(props) {
+  // declase a new state variable called count
+  const [count, setCount] = useState(0)
+  // similar to componentDidMount and componentDidUpdate
+  useEffect(() => {
+    // update the document title using the browser API
+    document.title = `You clicked ${count} times`;
+  })
   return (
     <div>
-      <h1>My awesome counter</h1>
+      <h1>You clicked {count} times</h1>
+      <button onClick={() => { setCount(count + 1) }}>
+        Click me babe
+      </button>
     </div>
   )
 }
